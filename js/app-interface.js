f46ade5dfe9f8445ae83cf0de9a170efa032807b
kyle tenzeldam <ktenzel@gmail.com>
2018-02-21 22:14:25 -0800

    rework name request

--- js/app-interface.js
+++ js/app-interface.js
@@ -1,4 +1,4 @@
-import {DocName} from './../js/docName.js';
+import {docName} from './../js/docName.js';
 import {docQuery} from './../js/docQueryb.js';
 
 $(document).ready(function() {
@@ -7,25 +7,28 @@ $(document).ready(function() {
 // let body.data = [];
 // let nameArray = [];
 
-  // $('#nameForm').submit(function(event){
-  //   $("#showDoctors").empty();
-  //   event.preventDefault();
-  //   let firstName = $('#firstName').val();
-  //   let lastName = $('#lastName').val();
-  //   debugger;
-  //   info.getDocName(firstName, lastName);
-  //   for (var i = 0; i < info.docName.length; i++) {
-  //   if(info.docName.length > 0) {
-  //     $('#showDoctors').append(`<div class="list"><strong>
-  //     ${info.docName[i].profile.first_name} ${info.docName[i].profile.last_name} </strong><br>${info.docName[i].practices[0].visit_address.street}<br>
-  //     ${info.docName[i].practices[0].visit_address.city} ${info.docName[i].practices[0].visit_address.state}, ${info.docName[i].practices[0].visit_address.zip}
-  //     <br>${info.docName[i].practices[0].phones[0].number}<br>Accepting new patients: ${info.docName[i].practices[0].accepts_new_patients}
-  //     <br>Website: ${info.docName[i].practices[0].website}<br></div>`);
-  //   } else {
-  //     $('.showErrors').append('<div class="list">No doctors availabe in your area fit that request.</div>');
-  //     }
-  //   };
-  // });
+  $('#nameForm').submit(function(event){
+    $("#showDoctors").empty();
+    $("#showErrors").empty();
+    event.preventDefault();
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let nameQuery = 
+    info.getDocName(firstName, lastName);
+    for (var i = 0; i < info.docName.length; i++) {
+    if(info.docName.length > 0) {
+      $('#showDoctors').append(`<div class="list"><strong>
+      ${info.docName[i].profile.first_name} ${info.docName[i].profile.last_name} </strong><br>${info.docName[i].practices[0].visit_address.street}<br>
+      ${info.docName[i].practices[0].visit_address.city} ${info.docName[i].practices[0].visit_address.state}, ${info.docName[i].practices[0].visit_address.zip}
+      <br>${info.docName[i].practices[0].phones[0].number}<br>Accepting new patients: ${info.docName[i].practices[0].accepts_new_patients}
+      <br>Website: ${info.docName[i].practices[0].website}<br></div>`);
+    } else {
+      $('.showErrors').append('<div class="list">No doctors availabe in your area fit that request.</div>');
+      }
+    };
+  });
+
+
   $('#queryForm').submit(function(event){
   $("#showDoctors").empty();
   $("#showErrors").empty();
--- js/docName.js
+++ js/docName.js
@@ -1,17 +1,17 @@
 var apiKey = require('./../.env').apiKey;
+//
+// export class DocName{
+//
+//   constructor() {
+//     // this.docName = [];
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
 
-export class DocName{
-
-  constructor() {
-    // this.docName = [];
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-getDocName(firstName, lastName) {
-  let docName = [];
-  let that = this;
-  let apiCall = new Promise(function(resolve, reject) {
+export let docName(firstName, lastName) {
+  // let docName = [];
+  // let that = this;
+  return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
     let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=or-portland&user_location=45.523%2C-122.676&skip=0&limit=10&user_key=${apiKey}`;
     console.log(url);
@@ -25,13 +25,13 @@ getDocName(firstName, lastName) {
     request.open("GET", url, true);
     request.send();
     });
-    apiCall.then(function(response) {
-    let body = JSON.parse(response);
-    for (var i = 0; i < body.data.length; i++) {
-      if(body.data.length > 0){
-        docName.push(body.data[i]);
-      }
-    }
-  });
-  }
-}
+    // apiCall.then(function(response) {
+    // let body = JSON.parse(response);
+    // for (var i = 0; i < body.data.length; i++) {
+    //   if(body.data.length > 0){
+    //     docName.push(body.data[i]);
+    //   }
+    // }
+  // });
+  // }
+};
